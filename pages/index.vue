9d8281e420a26e53cb171eea8e9a8d6668d31e22
Rachid LAASRI <61662210+rlaasri@users.noreply.github.com>
2023-02-06 17:01:18 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-white">
-    <sections-collections01 v-if="$settings.sections.collections.active"></sections-collections01>
-    <sections-products :key="1" :data-key="1" v-if="$settings.sections[`products_${1}`].active" :section="$settings.sections[`products_${1}`]"></sections-products>
-    <sections-collections02 v-if="$settings.sections.collections1.active"></sections-collections02>
+    <!-- <sections-collections01 v-if="$settings.sections.collections.active"></sections-collections01>  -->
+    <!-- <sections-products :key="1" :data-key="1" v-if="$settings.sections[`products_${1}`].active" :section="$settings.sections[`products_${1}`]"></sections-products>  -->
+    <!--  <sections-collections02 v-if="$settings.sections.collections1.active"></sections-collections02>  -->
     <template v-for="i in 4">
       <sections-products :key="i+1" :data-key="i+1" v-if="$settings.sections[`products_${i+1}`].active" :section="$settings.sections[`products_${i+1}`]"></sections-products>
     </template>
