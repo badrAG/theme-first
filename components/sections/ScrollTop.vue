c2cc4c112b36fd96479f8943b4d8b1073c152b1d
Abdelali Rafik <Abdelali-ErRafik>
2023-02-20 11:57:33 +0100

    DEV :: add change image to service

--- components/sections/ScrollTop.vue
+++ components/sections/ScrollTop.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Scroll to top -->
-    <div v-if="showButton" @click="scrollToTop" class="text-white inline-flex items-center justify-center fixed bottom-28 right-16 h-11 w-11 z-50 rounded-full box-shadow scale cursor-pointer bg-primary  hover:opacity-90">
+    <div v-if="showButton" @click="scrollToTop" class="text-white inline-flex items-center justify-center fixed bottom-20 right-10 md:bottom-28 md:right-16 h-11 w-11 z-50 rounded-full box-shadow scale cursor-pointer bg-primary  hover:opacity-90">
         <fa :icon="['fa', 'angle-up']" ></fa>
     </div>
     <!-- Scroll to top -->
--- components/sections/Services.vue
+++ components/sections/Services.vue
@@ -7,7 +7,7 @@
             <div class="service flex w-full sm:w-1/2 lg:w-1/4 p-2" v-for="(item, i) in items" :key="i">
                 <div class="w-full bg-white border border-gray-200 rounded-xl transition-all ease-linear box-shadow-x-hover overflow-hidden">
                     <nuxt-link :to="item.url" class="flex items-center">
-                        <si-image width="80" height="80" class="w-20 h-20 p-2 object-cover opacity-80" :src="item.image ? item.image.src : null" :alt="item.text"/>
+                        <si-image width="80" height="80" class="w-20 h-20 p-2 object-cover opacity-80 text-red-600" :src="item.image ? item.image.src : item.defaultImage.src" :alt="item.text"/>
                         <div class="mx-2">
                             <b class="text-base font-bold mb-1">{{ item.text }}</b>
                             <p class="text-base font-normal mt-1">{{ item.description }}</p>
@@ -25,25 +25,37 @@ export default {
         return {
             items: [
                 {
-                    image: { src: 'https://cdn-icons-png.flaticon.com/512/7610/7610711.png' },
+                    image : this.$settings.sections.services.items.delivery.image ,
+                    defaultImage: { 
+                        src: 'https://cdn-icons-png.flaticon.com/512/7610/7610711.png'
+                    },
                     text: this.$settings.sections.services.items.delivery.text,
                     url: this.$settings.sections.services.items.delivery.url,
                     description: this.$settings.sections.services.items.delivery.description,
                 },
                 {
-                    image: { src: 'https://cdn-icons-png.flaticon.com/512/3257/3257683.png' },
+                    image : this.$settings.sections.services.items.satisfaction.image ,
+                    defaultImage: { 
+                        src: 'https://cdn-icons-png.flaticon.com/512/3257/3257683.png' 
+                    },
                     text: this.$settings.sections.services.items.satisfaction.text,
                     url: this.$settings.sections.services.items.satisfaction.url,
                     description: this.$settings.sections.services.items.satisfaction.description,
                 },
                 {
-                    image: { src: 'https://cdn-icons-png.flaticon.com/512/1534/1534216.png' },
+                    image : this.$settings.sections.services.items.quality.image ,
+                    defaultImage: { 
+                        src: 'https://cdn-icons-png.flaticon.com/512/1534/1534216.png' 
+                    },
                     text: this.$settings.sections.services.items.quality.text,
                     url: this.$settings.sections.services.items.quality.url,
                     description: this.$settings.sections.services.items.quality.description,
                 },
                 {
-                    image: { src: 'https://cdn-icons-png.flaticon.com/512/950/950299.png' },
+                    image : this.$settings.sections.services.items.support.image ,
+                    defaultImage: { 
+                        src: 'https://cdn-icons-png.flaticon.com/512/950/950299.png' 
+                    },
                     text: this.$settings.sections.services.items.support.text,
                     url: this.$settings.sections.services.items.support.url,
                     description: this.$settings.sections.services.items.support.description,
