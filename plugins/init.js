0a3c6edbc14705e9b6db150306094c25cad3c4d5
badrag <badrag6@gmail.com>
2023-06-17 01:41:36 +0100

    PROD :: pixel

--- plugins/init.js
+++ plugins/init.js
@@ -189,7 +189,15 @@ export default async function ({ $axios, $http ,route, $tools, $storeino, store,
           }
         }
         if(route.name == 'thanks' && route.query.pixel){
-          window.snapPurchase({});
+         
+      let pixelData = JSON.parse(this.$route.query.pixel);
+          window.snapPurchase({
+            price: pixelData.total,
+            transaction_id: route.query.code,
+            currency: this.$store.state._current_currency && this.$store.state._current_currency.code ? this.$store.state._current_currency.code : "",
+            item_ids:pixelData.content_ids,
+            click_id:route.query.ScCid ? route.query.ScCid  : null
+          });
         }
       }
       
