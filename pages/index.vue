6a7582b90909cef7dd75d8fbd2e18edb73bdf49d
Abdelali-ErRafik <abdelali.rafik11@gmail.com>
2023-06-26 12:52:32 +0100

    PROD :: add home classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <sections-top-collections v-if="$settings.sections.top_collections.active"></sections-top-collections>
-    <sections-products :key="1" :data-key="1" v-if="$settings.sections[`products_${1}`].active" :section="$settings.sections[`products_${1}`]"></sections-products>
-    <sections-center-collections v-if="$settings.sections.center_collections.active"></sections-center-collections>
+  <div class="home-page">
+    <sections-top-collections class="home-top-collections" v-if="$settings.sections.top_collections.active"></sections-top-collections>
+    <sections-products :class="`home-products-${1}`" :key="1" :data-key="1" v-if="$settings.sections[`products_${1}`].active" :section="$settings.sections[`products_${1}`]"></sections-products>
+    <sections-center-collections class="home-center-collections" v-if="$settings.sections.center_collections.active"></sections-center-collections>
     <template v-for="i in 4">
-      <sections-products :key="i+1" :data-key="i+1" v-if="$settings.sections[`products_${i+1}`].active" :section="$settings.sections[`products_${i+1}`]"></sections-products>
+      <sections-products :class="`home-products-${i+1}`" :key="i+1" :data-key="i+1" v-if="$settings.sections[`products_${i+1}`].active" :section="$settings.sections[`products_${i+1}`]"></sections-products>
     </template>
-    <sections-collections-products v-if="$settings.sections.collections_products.active" ></sections-collections-products>
-    <sections-bottom-collections v-if="$settings.sections.bottom_collections.active"></sections-bottom-collections>
-    <sections-services v-if="$settings.sections.services.active"></sections-services>
+    <sections-collections-products class="home-home-collections-products" v-if="$settings.sections.collections_products.active" ></sections-collections-products>
+    <sections-bottom-collections class="home-bottom-collections" v-if="$settings.sections.bottom_collections.active"></sections-bottom-collections>
+    <sections-services class="home-services" v-if="$settings.sections.services.active"></sections-services>
     <div class="container">
-      <si-app-loader  placement="BEFORE_HOME_FOOTER"/>
+      <si-app-loader placement="BEFORE_HOME_FOOTER"/>
     </div>
-    <sections-testimonial v-if="$settings.sections.testimonial.active" ></sections-testimonial>
+    <sections-testimonial class="home-testimonial" v-if="$settings.sections.testimonial.active" ></sections-testimonial>
   </div>
 </template>
 
