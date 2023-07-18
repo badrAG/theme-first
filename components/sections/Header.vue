dd14e98e610f7f013bc0b42ccee1c8d5d101ac3d
Abdelali-ErRafik <abdelali.rafik11@gmail.com>
2023-07-18 16:45:38 +0100

    PROD :: fix header large menu

--- components/sections/Header.vue
+++ components/sections/Header.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <si-app-loader placement="BEFORE_HEADER"/>
-        <div class="header-bg header-text-color  border-b-2" :class="menu ? 'res-p' : 'py-2'">
+        <div class="border-b-2 header-bg header-text-color" :class="menu ? 'res-p' : 'py-2'">
             <header class="container flex items-center justify-between w-full ">
                 <!-- left content -->
                 <div class="mx-4">
                     <!-- logo -->
                     <div class="logo">
                         <router-link to="/">
-                            <si-image width="70" height="50" property="height" class="h-10 md:h-12 w-auto object-contain" :src="section.logo ? section.logo.src : $store.state.defaults.logo" alt="Store logo"/>
+                            <si-image width="70" height="50" property="height" class="object-contain w-auto h-10 md:h-12" :src="section.logo ? section.logo.src : $store.state.defaults.logo" alt="Store logo"/>
                         </router-link>
                     </div>
                     <!-- logo -->
@@ -17,18 +17,18 @@
                 <!-- center content -->
                 <div class="flex items-center " :class="$settings.sections.header.icons.search? 'w-full active-search':'unactive-search'">
                     <!-- show menu botton  -->
-                    <div class="headermenu-button mx-1 p-3 rounded-full hidden transition-all ease-in-out delay-150 hover-bg">
+                    <div class="hidden p-3 mx-1 transition-all ease-in-out delay-150 rounded-full headermenu-button hover-bg">
                         <button @click="$store.state.showHeaderMenu = !$store.state.showHeaderMenu" aria-label="Search button" class="flex flex-col justify-center">
                             <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-5 h-5 translate svg-color "><g class="fa-group"><path fill="currentColor" d="M16 288h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16z" class="fa-secondary"></path><path fill="currentColor" d="M432 384H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H16A16 16 0 0 0 0 80v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16z"></path></g></svg>
                         </button>
                     </div>
                     <!-- show menu botton  -->
                     <!-- search menu -->
-                    <div v-if="$settings.sections.header.icons.search" class="search w-full mr-4">
-                        <form @submit.prevent="search" class="flex items-center bg-gray-100 focus:bg-white rounded-full border-2 header-bg-border" action="/shop?">
-                            <input  v-model="q" class="b1 flex items-center rounded-l-full bg-transparent outline-none w-full py-2 px-4 text-base focus:bg-white" :placeholder="$settings.sections.header.search.text" type="search" name="q">
-                            <button class="b2 flex items-center py-3 px-4 rounded-r-full transition-all ease-in-out delay-150" aria-label="Search button">
-                                <fa class="icon text-lg header-text-color"  :icon="['fa', 'magnifying-glass']"></fa>
+                    <div v-if="$settings.sections.header.icons.search" class="w-full mr-4 search">
+                        <form @submit.prevent="search" class="flex items-center bg-gray-100 border-2 rounded-full focus:bg-white header-bg-border" action="/shop?">
+                            <input  v-model="q" class="flex items-center w-full px-4 py-2 text-base bg-transparent rounded-l-full outline-none b1 focus:bg-white" :placeholder="$settings.sections.header.search.text" type="search" name="q">
+                            <button class="flex items-center px-4 py-3 transition-all ease-in-out delay-150 rounded-r-full b2" aria-label="Search button">
+                                <fa class="text-lg icon header-text-color"  :icon="['fa', 'magnifying-glass']"></fa>
                             </button>
                         </form>
                     </div>
@@ -37,21 +37,21 @@
                 <!-- center content -->
                 <!-- right content -->
                 <!-- icons -->
-                <div class="icons flex-shrink-0 mx-1">
+                <div class="flex-shrink-0 mx-1 icons">
                     <div class="flex items-center content-between">
                     <!-- courency and language dropdown  -->
-                    <div class="lang flex items-center justify-between relative">
+                    <div class="relative flex items-center justify-between lang">
                         <ul v-for="(item, i) in otherMenu" :key="i" class="flex flex-col" @mouseover="activeId = activeId = item._id" @mouseleave="activeId = activeId = null">
-                            <li class="cursor-pointer transition-all ease-in-out delay-150 hover-bg rounded-full flex items-center justify-between">
-                                <a class="p-2 flex items-center" :href="item.url">
-                                    <div class="text-sm font-bold mx-1">{{ item.text }}</div>
+                            <li class="flex items-center justify-between transition-all ease-in-out delay-150 rounded-full cursor-pointer hover-bg">
+                                <a class="flex items-center p-2" :href="item.url">
+                                    <div class="mx-1 text-sm font-bold">{{ item.text }}</div>
                                     <button>
                                         <fa class="text-xs transform" v-if="item.childrens && item.childrens.length > 0"  :icon="['fa', 'angle-down']" :class="[activeId==item._id ? 'rotate-180 transition-all delay-150 ease-linear' : ''] "></fa>
                                     </button>
                                 </a>
                             </li>
                             <transition name="slide">
-                              <div class="w-24 absolute top-full header-bg shadow-lg z-20 border p-2" :class="menu? 'top-10.5' : 'top-3'" v-if="item._id == activeId">
+                              <div class="absolute z-20 w-24 p-2 border shadow-lg top-full header-bg" :class="menu? 'top-10.5' : 'top-3'" v-if="item._id == activeId">
                                   <div class="pb-1" v-for="(item,i) in item.childrens" :key="i" >
                                       <a class="text-sm font-normal hover:underline" :href="item.url">{{item.text}}</a>
                                       <ul v-if="item.childrens && item.childrens.length > 0">
@@ -67,19 +67,19 @@
                             </ul>
                         </div>
                         <!-- courency and language dropdown  -->
-                        <div v-if="$settings.sections.header.icons.account" class="user p-3 rounded-full transition-all ease-in-out delay-150 hover-bg">
+                        <div v-if="$settings.sections.header.icons.account" class="p-3 transition-all ease-in-out delay-150 rounded-full user hover-bg">
                             <router-link  to="/account/orders" title="Account" class="flex flex-col justify-center">
                                 <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-5 h-5 svg-color"><path  d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z" class=""></path></svg>
                             </router-link>
                         </div>
-                        <div v-if="$settings.sections.header.icons.wishlist" class="heart p-3 rounded-full transition-all ease-in-out delay-150 hover-bg">
-                            <router-link  to="/wishlist" title="Wishlist" class="flex flex-col justify-center relative">
+                        <div v-if="$settings.sections.header.icons.wishlist" class="p-3 transition-all ease-in-out delay-150 rounded-full heart hover-bg">
+                            <router-link  to="/wishlist" title="Wishlist" class="relative flex flex-col justify-center">
                                 <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="heart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 svg-color"><path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z"></path></svg>
                                 <small v-if="$store.state.wishlist.length>0"  class="border-2 border-white -top-3 -right-2.5 rounded-full absolute w-5 h-5 p-1 bg-orange text-white flex justify-center items-center text-md font-normal">{{ $store.state.wishlist.length }}</small>
                             </router-link>
                         </div>
-                        <div v-if="$settings.sections.header.icons.cart" class="cart p-3 rounded-full transition-all ease-in-out delay-150 hover-bg">
-                            <router-link  to="/cart" title="Cart" id="cart-icon" class="flex flex-col justify-center relative">
+                        <div v-if="$settings.sections.header.icons.cart" class="p-3 transition-all ease-in-out delay-150 rounded-full cart hover-bg">
+                            <router-link  to="/cart" title="Cart" id="cart-icon" class="relative flex flex-col justify-center">
                                 <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="shopping-cart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 svg-color"><path d="M9 22a2 2 0 100-4 2 2 0 000 4zm7 0a2 2 0 100-4 2 2 0 000 4zm5-17H5.665l-.687-3.21A1 1 0 004 1H1a1 1 0 000 2h2.191l2.831 13.21a.962.962 0 00.064.159c.017.053.038.105.063.155a.979.979 0 00.133.153.926.926 0 00.088.1c.058.041.12.077.185.105.034.022.07.042.107.06A.993.993 0 007 17h11a1 1 0 00.958-.713l3-10A1.001 1.001 0 0021 5zm-2.244 5H16V7h3.656l-.9 3zM7.819 15l-.6-3H9v3H7.819zM11 12h3v3h-3v-3zm0-2V7h3v3h-3zM9 7v3H6.82l-.6-3H9zm8.256 8H16v-3h2.156l-.9 3z"></path></svg>
                                 <!-- v-if="$store.state.cart.length>0" -->
                                 <small class="border-2 border-white -top-3 -right-2.5 rounded-full absolute w-5 h-5 bg-orange text-white flex justify-center items-center text-md font-normal">{{ $store.state.cart.length }}</small>
@@ -91,19 +91,19 @@
                 <!-- right content -->
             </header>
             <!-- Nav Bar -->
-            <nav v-if="menu" class="flex items-center justify-center font-normal text-sm ">
+            <nav v-if="menu" class="flex items-center justify-center overflow-x-auto text-sm font-normal scroll-x">
                 <ul v-for="(item, i) in menu.items" :key="i" class="flex flex-col" @mouseover="activeId = activeId = item._id" @mouseleave="activeId = activeId = null">
-                    <li class="flex items-center p-2 mx-2 border-b-2 border-transparent hover:border-black transition-all delay-150 ease-linear">
+                    <li class="flex items-center p-2 mx-2 transition-all ease-linear delay-150 border-b-2 border-transparent hover:border-black">
                         <router-link :to="item.url">
-                            {{ item.text }}
+                            <span class="whitespace-nowrap">{{ item.text }}</span>
                         </router-link>
-                        <button v-if="item.childrens.length > 0" class="chivron ml-1"  @click="activeId = activeId != item._id ? item._id : null">
+                        <button v-if="item.childrens.length > 0" class="ml-1 chivron"  @click="activeId = activeId != item._id ? item._id : null">
                             <fa class="text-xs transform" v-if="item.childrens && item.childrens.length > 0"  :icon="['fa', 'angle-down']" :class="[activeId==item._id ? 'rotate-180 transition-all delay-150 ease-linear' : ''] "></fa>
                         </button>
                     </li>
                     <transition name="slide">
                         <div class="relative">
-                            <div v-if="item._id == activeId && item.childrens.length > 0" class="w-40 top-full absolute shadow-lg z-20 border header-bg p-2">
+                            <div v-if="item._id == activeId && item.childrens.length > 0" class="absolute z-20 w-40 p-2 border shadow-lg top-full header-bg">
                                 <div class="py-1" v-for="(item,i) in item.childrens" :key="i" @mouseover="subItems = subItems = item.collectionId" @mouseleave="subItems = subItems = null">
                                     <div class="flex items-center justify-between">
                                         <router-link class="text-sm font-normal hover:underline" :to="item.url">
