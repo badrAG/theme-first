ee18bcbd9bc605b860afb60c4fffba690312998c
Abdelali-ErRafik <abdelali.rafik11@gmail.com>
2023-09-12 18:40:45 +0100

    DEV :: Install Performance Packages

--- store/index.js
+++ store/index.js
@@ -23,7 +23,7 @@ const createStore = () => {
           domain: 'www.storeino.com',
           apps: [],
           // Dev Token
-          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdG9yZSI6eyJfaWQiOiI2NDc1YjM1MGU3NTI3MTIyYzc5M2JjOTkifSwic291cmNlIjoiVEhFTUVTIiwiaWF0IjoxNjk0MTY3NDA1LCJleHAiOjE3MjU3MDM0MDV9.FfJzLaRtmVl89VOG_i87BG2OZYBz391tQWNw8HEDNSo',
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdG9yZSI6eyJfaWQiOiI2NTAwOTdhMDgzYjllZjFiZjkxNTZkZmEiLCJuYW1lIjoiYmF6emFyLXRoZW1lIiwic3ViZG9tYWluIjoiYmF6emFyLXRoZW1lLnN0b3JlaW5vLndvcmxkIn0sInVzZXIiOnsiX2lkIjoiNjI1NDNiNDM1YWY3OGYwMDQ0YjU3YzdlIiwiZmlyc3RuYW1lIjoiYWJkZWxhbGkiLCJsYXN0bmFtZSI6InJhZmlrIiwiZW1haWwiOiJhYmRlbGFsaS5yYWZpazExQGdtYWlsLmNvbSJ9LCJjb21wYW55Ijp7InN0YXR1cyI6IlVOQ09NUExFVEVEIiwiX2lkIjoiNjI1NDNiNDM1YWY3OGYwMDQ0YjU3YzdjIiwibmFtZSI6InNvdTlpbm8ifSwidG9rZW5JZCI6IjExNTMzM01ER01JQjEzMzMzM0lLUlRMRCIsImlhdCI6MTY5NDUzNzc5MiwiZXhwIjoxNjk1NDAxNzkyfQ.ZAyS-tu98FDwZFXrDpoL49Ai22a2MMre_BrEzQ4AGS0',
           // Prod Token
           // token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdG9yZSI6eyJfaWQiOiI2MjYxMmZhN2I4ODZkYTAwYTRkNWZmMmYifSwic291cmNlIjoiVEhFTUVTIiwiaWF0IjoxNjkyMTczODM0LCJleHAiOjE3MjM3MDk4MzR9.Hy27CGIsqpU5Hn3sp-X47P38x090kBxAurTwmVfN55w',
           primary: {
