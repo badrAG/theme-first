539c4caff878e29b1855805e1704c97da208b2d5
Abdelali-ErRafik <abdelali.rafik11@gmail.com>
2024-02-06 18:58:56 +0100

    DEV :: Update Maintenance Page

--- components/sections/Maintenance.vue
+++ components/sections/Maintenance.vue
@@ -13,16 +13,17 @@
           <!--  -->
           <form class="flex items-center justify-center gap-2" @submit.prevent="verifier">
             <!--  -->
-            <div class="flex text-base border border-gray-300 rounded-md p-2 text-gray-400 bg-gray-100 hover:bg-gray-200 transition-all">
+            <div class="flex text-base border border-gray-300 rounded-md p-2 text-gray-500 bg-gray-100 hover:bg-gray-200 transition-all">
               <!--  -->
               <input class="bg-transparent outline-none" type="text" v-model="code" placeholder="••••••••" />
               <!--  -->
             </div>
             <!--  -->
-            <button class="bg-blue-500 border border-blue-300 rounded-md p-2 opacity-60 hover:opacity-100" type="submit">
+            <button class="bg-blue-500 border border-blue-300 rounded-md p-2 opacity-70 hover:opacity-100 transition-all" type="submit">
               <!--  -->
-              <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24">
-                <path d="M15,9.25H9c-.086,0-.165.021-.25.025V7A3.254,3.254,0,0,1,12,3.75a3.417,3.417,0,0,1,3.273,2.437.751.751,0,0,0,1.454-.374A4.893,4.893,0,0,0,12,2.25,4.756,4.756,0,0,0,7.25,7V9.594A4.749,4.749,0,0,0,4.25,14v3A4.756,4.756,0,0,0,9,21.75h6A4.756,4.756,0,0,0,19.75,17V14A4.756,4.756,0,0,0,15,9.25ZM18.25,17A3.254,3.254,0,0,1,15,20.25H9A3.254,3.254,0,0,1,5.75,17V14A3.254,3.254,0,0,1,9,10.75h6A3.254,3.254,0,0,1,18.25,14Zm-5.5-2v1a.75.75,0,0,1-1.5,0V15a.75.75,0,0,1,1.5,0Z" fill="currentColor"/>
+              <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="20px" height="200px" viewBox="0 0 24 24" fill="none">
+                <path d="M11 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                <path d="M6 10V8C6 7.65929 6.0284 7.32521 6.08296 7M17.811 6.5C17.1449 3.91216 14.7958 2 12 2C10.223 2 8.62643 2.7725 7.52779 4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
               </svg>
               <!--  -->
             </button>
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,16 @@
         </div>
         <!--  -->
         <div v-else class="nuxt_page">
+            <!--  -->
+            <div class="fixed top-0 right-0 z-50" v-if="$settings.store_maintenance_unlocked">
+                <div class="bg-blue-500 p-2 cursor-pointer opacity-70 hover:opacity-100 transition-all" @click="closeStore()">
+                    <svg class="text-white w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none">
+                        <path d="M6 10V8C6 7.65929 6.0284 7.32521 6.08296 7M18 10V8C18 4.68629 15.3137 2 12 2C10.208 2 8.59942 2.78563 7.5 4.03126" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                        <path d="M11 22H8C5.17157 22 3.75736 22 2.87868 21.1213C2 20.2426 2 18.8284 2 16C2 13.1716 2 11.7574 2.87868 10.8787C3.75736 10 5.17157 10 8 10H16C18.8284 10 20.2426 10 21.1213 10.8787C22 11.7574 22 13.1716 22 16C22 18.8284 22 20.2426 21.1213 21.1213C20.2426 22 18.8284 22 16 22H15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+                    </svg>
+                </div>
+            </div>
+            <!--  -->
             <component :is="'style'">
                 :root{  
                     --primary-rgb: {{ rgb.r }}, {{rgb.g}}, {{ rgb.b }}; 
