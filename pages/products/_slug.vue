97eb088e39c620143c22ece01dbf96167396cccf
Abdelali-ErRafik <84728514+Abdelali-ErRafik@users.noreply.github.com>
2024-05-06 16:43:13 +0100

    DEV :: Fix Varaints

--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -134,7 +134,7 @@
                         <p class="mx-2 mb-2 text-sm font-normal text-gr">{{ item.description }}</p>
                         <!-- short description -->
                         <!-- variant -->
-                        <div v-show="!settings.checkout_required_fields.show_variant_on_checkout">
+                        <div v-show="!$settings.checkout_required_fields.show_variant_on_checkout">
                             <si-product-variants class="flex mx-2" v-if="item.type=='variable'" :options="item.options" :images="item.images" :variants="item.variants" @selected="variantSelected"></si-product-variants>
                         </div>
                         <!-- variant -->
